61d2fc48117991c140c496de6e3647267bdefdb2
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-06-07 23:54:51 +0700

    refactor(extractor): simplify icon extraction button logic and add logging
    Remove platform-specific button creation in favor of a simpler approach.
    Add logging to track saved icons for debugging purposes.

--- src/extractor/Main.java
+++ src/extractor/Main.java
@@ -4,15 +4,14 @@ import arc.*;
 import arc.files.Fi;
 import arc.graphics.Pixmap;
 import arc.graphics.PixmapIO;
-import arc.graphics.g2d.TextureRegion;
 import arc.struct.Seq;
+import arc.util.Log;
 import mindustry.*;
 import mindustry.ctype.Content;
 import mindustry.ctype.UnlockableContent;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.ui.fragments.MenuFragment.MenuButton;
 
 public class Main extends Mod {
 
@@ -22,17 +21,9 @@ public class Main extends Mod {
         iconDir.mkdirs();
 
         Events.on(ClientLoadEvent.class, e -> {
-            if (Vars.mobile) {
-                Vars.ui.menufrag.addButton("Extract Icon", Icon.map, () -> {
-                    outputContentSprites();
-                });
-            } else {
-                Vars.ui.menufrag.addButton(new MenuButton("Tools", Icon.wrench, () -> {
-                }, //
-                        new MenuButton(Core.bundle.format("Extract Icon"), Icon.map, () -> {
-                            outputContentSprites();
-                        })));
-            }
+            Vars.ui.menufrag.addButton("Extract Icon", Icon.map, () -> {
+                outputContentSprites();
+            });
         });
     }
 
@@ -41,11 +32,12 @@ public class Main extends Mod {
             for (Content content : contents) {
                 if (content instanceof UnlockableContent) {
                     String name = ((UnlockableContent) content).name;
-                    TextureRegion icon = ((UnlockableContent) content).fullIcon;
+                    var icon = ((UnlockableContent) content).fullIcon;
                     Fi fi = iconDir.child(name + ".png");
                     Pixmap pixmap = Core.atlas.getPixmap(icon).crop();
                     PixmapIO.writePng(fi, pixmap);
                     pixmap.dispose();
+                    Log.info("Saved " + name + " at " + fi.absolutePath());
                 }
             }
         }
